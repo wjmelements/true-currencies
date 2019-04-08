49f69db77fd234a278efc2326d593fced2acba43
William Morriss <wjmelements@gmail.com>
2019-04-07 17:58:26 -0700

    fix syntax

--- truffle.js
+++ truffle.js
@@ -11,7 +11,7 @@ module.exports = {
         enabled: true,
         runs: 20000
       }
-    }
+    },
     optimizer: {
       enabled: true,
       runs: 20000
@@ -25,7 +25,7 @@ module.exports = {
           enabled: true,
           runs: 20000
         }
-      }
+      },
       optimizer: {
         enabled: true,
         runs: 20000
