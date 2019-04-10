ea9eec6c53c5ba750dc5c1694d1b309e05e5a243
William Morriss <wjmelements@gmail.com>
2019-04-09 17:47:04 -0700

    clean up

--- contracts/ProvisionalTrueUSD.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.23;
-
-import "./TrueUSD.sol";
-import "./ProvisionalCompliantDepositTokenWithHook.sol";
-
-contract ProvisionalTrueUSD is TrueUSD, ProvisionalCompliantDepositTokenWithHook {
-}
