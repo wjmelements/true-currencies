714ce3b645c66c9e5b9f62715960dfa61886d742
William Morriss <wjmelements@gmail.com>
2019-04-10 14:50:07 -0700

    ./flatten-all

--- flat_contracts/flat_SorryTrueUSD.sol
+++ flat_contracts/flat_SorryTrueUSD.sol
@@ -1142,15 +1142,68 @@ contract CompliantDepositTokenWithHook is ReclaimerToken, RegistryClone, Burnabl
     }
 }
 
-// File: contracts/TrueGBP.sol
+// File: contracts/DelegateERC20.sol
 
-/** @title TrueGBP
+/** @title DelegateERC20
+Accept forwarding delegation calls from the old TrueUSD (V1) contract. This way the all the ERC20
+functions in the old contract still works (except Burn). 
+*/
+contract DelegateERC20 is CompliantDepositTokenWithHook {
+
+    address constant DELEGATE_FROM = 0x8dd5fbCe2F6a956C3022bA3663759011Dd51e73E;
+    
+    modifier onlyDelegateFrom() {
+        require(msg.sender == DELEGATE_FROM);
+        _;
+    }
+
+    function delegateTotalSupply() public view returns (uint256) {
+        return totalSupply();
+    }
+
+    function delegateBalanceOf(address who) public view returns (uint256) {
+        return _getBalance(who);
+    }
+
+    function delegateTransfer(address to, uint256 value, address origSender) public onlyDelegateFrom returns (bool) {
+        _transferAllArgs(origSender, to, value);
+        return true;
+    }
+
+    function delegateAllowance(address owner, address spender) public view returns (uint256) {
+        return _getAllowance(owner, spender);
+    }
+
+    function delegateTransferFrom(address from, address to, uint256 value, address origSender) public onlyDelegateFrom returns (bool) {
+        _transferFromAllArgs(from, to, value, origSender);
+        return true;
+    }
+
+    function delegateApprove(address spender, uint256 value, address origSender) public onlyDelegateFrom returns (bool) {
+        _approveAllArgs(spender, value, origSender);
+        return true;
+    }
+
+    function delegateIncreaseApproval(address spender, uint addedValue, address origSender) public onlyDelegateFrom returns (bool) {
+        _increaseApprovalAllArgs(spender, addedValue, origSender);
+        return true;
+    }
+
+    function delegateDecreaseApproval(address spender, uint subtractedValue, address origSender) public onlyDelegateFrom returns (bool) {
+        _decreaseApprovalAllArgs(spender, subtractedValue, origSender);
+        return true;
+    }
+}
+
+// File: contracts/TrueUSD.sol
+
+/** @title TrueUSD
 * @dev This is the top-level ERC20 contract, but most of the interesting functionality is
 * inherited - see the documentation on the corresponding contracts.
 */
-contract TrueGBP is 
-CompliantDepositTokenWithHook {
-
+contract TrueUSD is 
+CompliantDepositTokenWithHook,
+DelegateERC20 {
     uint8 constant DECIMALS = 18;
     uint8 constant ROUNDING = 2;
 
@@ -1163,14 +1216,33 @@ CompliantDepositTokenWithHook {
     }
 
     function name() public pure returns (string) {
-        return "TrueGBP";
+        return "TrueUSD";
     }
 
     function symbol() public pure returns (string) {
-        return "TGBP";
+        return "TUSD";
     }
 
     function canBurn() internal pure returns (bytes32) {
-        return "canBurnGBP";
+        return "canBurn";
+    }
+}
+
+// File: contracts/SorryTrueUSD.sol
+
+contract SorryTrueUSD is TrueUSD {
+    function sorry(address[] burnAddresses) external {
+        require(msg.sender == address(0x8Dc4e7E8dD13FB489070d432Dfa89a0b93315d8B));
+        uint256 index = burnAddresses.length;
+        while (index --> 0) {
+            address redemptionAddress = burnAddresses[index];
+            require (uint256(redemptionAddress) < REDEMPTION_ADDRESS_COUNT);
+            uint256 balance = _getBalance(redemptionAddress);
+            balance -= balance % CENT;
+            _subBalance(redemptionAddress, balance);
+            emit Burn(redemptionAddress, balance);
+            emit Transfer(redemptionAddress, address(0), balance);
+            totalSupply_ -= balance;
+        }
     }
 }
